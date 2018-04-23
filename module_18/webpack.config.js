a35dd0fe1e86c7eabefb09156f3f81a706ac83cb
Hubert K <kolcz.h@gmail.com>
2018-04-23 15:03:02 +0200

    Summary update, 23.04

---
+++ module_18/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+var env = process.env.NODE_ENV || 'development';
+
+
+console.log('NODE_ENV:', env);
+
+
+module.exports = {
+    entry: [
+        './client/index.js'],
+    output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    ]
+};
